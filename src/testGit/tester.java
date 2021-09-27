28cc2a807be89183d24138bdc7d74ff133a14a6d
s-Doyle <spencer_doyle@hotmail.com>
2021-09-27 20:40:25 +1000

    first commit

---
+++ src/testGit/tester.java
@@ -0,0 +1,14 @@
+package testGit;
+
+public class tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "String s1";
+		
+		System.out.println(s1);
+
+	}
+
+}
