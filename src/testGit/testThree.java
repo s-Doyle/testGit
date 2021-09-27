715c80a29691ff7dadc033411f090cdbeaae5d0e
s-Doyle <spencer_doyle@hotmail.com>
2021-09-27 21:18:03 +1000

    third

---
+++ src/testGit/testThree.java
@@ -0,0 +1,14 @@
+package testGit;
+
+public class testThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 13;
+		int y = 8;
+		
+		System.out.println(x + y);
+
+	}
+
+}
